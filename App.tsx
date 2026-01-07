ab27020f65fffb09bd3751314218c60f5904f59e
Olamide <bondpierce0@gmail.com>
2026-01-07 03:59:12 +0100

    UI: Remove AI Assistant floating button

--- App.tsx
+++ App.tsx
@@ -7,7 +7,6 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
-import AiAssistant from './components/AiAssistant';
 import BackToTop from './components/BackToTop';
 import ProjectDetails from './ProjectDetails';
 import WorkPage from './pages/WorkPage';
@@ -92,7 +91,6 @@ const App: React.FC = () => {
             <>
               <BackToTop />
               <WhatsAppButton />
-              <AiAssistant />
             </>
           } />
         </Routes>
